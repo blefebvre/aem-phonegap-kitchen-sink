384e9c3a0c9e8acd51fe0550bd46fa571658322f
Bruce Lefebvre <blefebvr@adobe.com>
2014-04-07 14:53:06 +0100

    Refactored package to include bundle

--- refresh-contentsync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-curl -u admin:admin -X POST -F action="clear" -F path="/content/phonegap/brucelefebvre/content/kitchen-sink/kitchen-sink-content-sync" -F "_charset_"="utf-8" http://localhost:4502/libs/cq/contentsync/console/configs.json
-curl -u admin:admin -X POST -F action="update" -F path="/content/phonegap/brucelefebvre/content/kitchen-sink/kitchen-sink-content-sync" -F "_charset_"="utf-8" http://localhost:4502/libs/cq/contentsync/console/configs.json
