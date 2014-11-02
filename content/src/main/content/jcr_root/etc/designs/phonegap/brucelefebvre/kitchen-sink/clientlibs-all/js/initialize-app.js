128b24d86f0409e4a8be4d80362125934050f71c
Bruce Lefebvre <blefebvr@adobe.com>
2014-11-01 23:01:57 -0400

    Upgraded Kitchen Sink to take advantage of 6.0.1 features

---
+++ content/src/main/content/jcr_root/etc/designs/phonegap/brucelefebvre/kitchen-sink/clientlibs-all/js/initialize-app.js
@@ -0,0 +1,8 @@
+;(function(CQ, document, undefined) {
+
+    // iOS 7 status bar shim to prevent content from flowing beneath the transparent status bar
+    if(navigator.userAgent.match(/iPhone OS 7/)) {
+        document.body.className = document.body.className + ' ios7';
+    }
+
+})(CQ, document);
